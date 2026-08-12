ccdc5900b46320bfb31690c492d358ff3787464b
anhle <anhle261137@gmail.com>
2026-08-12 14:20:13 +0200

    remove base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://skitzofeinic.github.io',
-  base: 'skitzofeinic.github.io',
   vite: {
     plugins: [tailwindcss()]
   }
